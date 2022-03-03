cea9631fa84ccf656e25f24dd84ecd7f26301b22
Yehor Kozyr <y.kozyr@student.tudelft.nl>
2022-03-03 10:17:33 +0100

    Fixed buttons, added functionality to quit button
    Fixed buttons size in bottom block
    Added functionality to quit button, now it actually works and quits the application

--- client/src/main/java/client/scenes/SplashCtrl.java
+++ client/src/main/java/client/scenes/SplashCtrl.java
@@ -1,4 +1,11 @@
 package client.scenes;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class SplashCtrl {
+    public void quitAction(ActionEvent event){
+        ((Stage)(((Button)event.getSource()).getScene().getWindow())).close();
+    }
 }
