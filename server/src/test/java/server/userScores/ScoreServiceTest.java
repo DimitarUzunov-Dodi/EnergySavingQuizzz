ce5fbaf47992e6b9eeac408ff0e8e2f2bbc0be93
Alexandru Cristian Cazacu <a.c.cazacu@student.tudelft.nl>
2022-03-06 23:31:12 +0100

    Checkstyle

--- server/src/test/java/server/userScores/ScoreServiceTest.java
+++ server/src/test/java/server/userScores/ScoreServiceTest.java
@@ -1,11 +1,10 @@
 package server.userScores;
 
 
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ScoreServiceTest {
