d8dc44727b44fa571b6fa7270d632e56d2ed490f
Yehor Kozyr <y.kozyr@student.tudelft.nl>
2022-03-12 21:27:16 +0100

    Added endpoint for restarting the server
    Added endpoint and a method for restarting the server

---
+++ server/src/main/java/server/api/AdminController.java
@@ -0,0 +1,18 @@
+package server.api;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import server.Main;
+
+@RestController
+@RequestMapping("/api/admin")
+public class AdminController {
+
+    @PostMapping
+    public ResponseEntity<?> restartServer(){
+        Main.restart();
+        return ResponseEntity.ok("Restarted successfully");
+    }
+}
