a25e23d7f39f8cc074d5e5b0fc34e1320636a3c9
Chaan van den Oudenhoven <C.vandenOudenhoven@student.tudelft.nl>
2022-03-13 10:24:30 +0100

    gave buttons an id

--- client/src/main/java/client/scenes/GamePageController.java
+++ client/src/main/java/client/scenes/GamePageController.java
@@ -7,12 +7,14 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class GamePageController implements Initializable {
@@ -29,12 +31,26 @@ public class GamePageController implements Initializable {
     @FXML
     private ListView<String> currentLeaderboard;
 
+
+    @FXML
+    private Button button1;
+
+    @FXML
+    private Button button2;
+
+    @FXML
+    private Button button3;
+
+    @FXML
+    private Button button4;
     @FXML
     private Text Question_text;
 
 
 
     String[] names = {"foo", "bar", "test"};
+    private ArrayList<Button> button_List = new ArrayList<>();
+
 
     @Inject
     public GamePageController(ServerUtils server, MainCtrl mainCtrl) {
@@ -45,7 +61,10 @@ public class GamePageController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
        // Question_text = new Text("foo");
-
+        button_List.add(button1);
+        button_List.add(button2);
+        button_List.add(button3);
+        button_List.add(button4);
 
         Question_text.setText("foo");
 
