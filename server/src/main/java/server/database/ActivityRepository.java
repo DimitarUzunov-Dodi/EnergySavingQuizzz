8db426529c5b500e1ea0a162416a7dccb1044bce
Alexandru Cristian Cazacu <a.c.cazacu@student.tudelft.nl>
2022-03-13 20:46:28 +0100

    Created Activity repo with custom queries

---
+++ server/src/main/java/server/database/ActivityRepository.java
@@ -0,0 +1,24 @@
+package server.database;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import commons.Activity;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ActivityRepository extends JpaRepository<Activity, Long> {
+
+    @Query(
+            value = "SELECT * from question order by RANDOM() limit 1",
+            nativeQuery = true
+    )
+    Optional<Activity> getOneRandom();
+
+    @Query(
+            value = "SELECT * from question order by RANDOM() limit 3",
+            nativeQuery = true
+    )
+    List<Activity> getThreeRandom();
+
+}
