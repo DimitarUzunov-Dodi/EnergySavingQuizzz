4df0f5799e650550784d917058be55006ebd6a7f
Chaan van den Oudenhoven <C.vandenOudenhoven@student.tudelft.nl>
2022-03-20 21:24:02 +0100

    removed unused imports

--- client/src/main/java/client/utils/ServerUtils.java
+++ client/src/main/java/client/utils/ServerUtils.java
@@ -15,23 +15,17 @@
  */
 package client.utils;
 
-import org.springframework.messaging.converter.MappingJackson2MessageConverter;
-import org.springframework.messaging.simp.stomp.StompFrameHandler;
-import org.springframework.messaging.simp.stomp.StompHeaders;
-import org.springframework.messaging.simp.stomp.StompSession;
+
 import commons.LeaderboardEntry;
 import commons.ScoreRecord;
 import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.core.GenericType;
 import org.glassfish.jersey.client.ClientConfig;
-import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
-import org.springframework.web.socket.client.standard.StandardWebSocketClient;
-import org.springframework.web.socket.messaging.WebSocketStompClient;
 
-import java.lang.reflect.Type;
+
+
 import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.function.Consumer;
+
 
 import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
 
