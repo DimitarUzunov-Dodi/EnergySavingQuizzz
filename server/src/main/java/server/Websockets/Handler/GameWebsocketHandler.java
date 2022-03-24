226db0af4cafe0aeef5c99879aebe3f5174b2743
Alexandru Cristian Cazacu <a.c.cazacu@student.tudelft.nl>
2022-03-24 18:25:33 +0100

    Hotfix

--- server/src/main/java/server/Websockets/Handler/GameWebsocketHandler.java
+++ server/src/main/java/server/Websockets/Handler/GameWebsocketHandler.java
@@ -2,7 +2,6 @@ package server.websockets.handler;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
