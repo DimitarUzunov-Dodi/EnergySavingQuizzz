a3a21b6da60fe7bc951b2ddd8449659d32645647
Alexandru Cristian Cazacu <a.c.cazacu@student.tudelft.nl>
2022-03-25 22:00:17 +0100

    Third type of question

---
+++ commons/src/main/java/commons/QuestionTypeD.java
@@ -0,0 +1,40 @@
+package commons;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+public class QuestionTypeD extends Question {
+
+    private final int questionType;
+    private final Activity activity;
+
+    /**
+     * Constructor.
+     * @param activity activity
+     */
+    public QuestionTypeD(Activity activity) {
+        this.questionType = 3;
+        this.activity = activity;
+    }
+
+    public int getQuestionType() {
+        return questionType;
+    }
+
+    public Activity getActivity() {
+        return activity;
+    }
+
+    @Override
+    public String displayText() {
+        return "How much energy in KWh does the following activity take?";
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .append("questionType", questionType)
+                .append("activity", activity)
+                .append("/n")
+                .toString();
+    }
+}
