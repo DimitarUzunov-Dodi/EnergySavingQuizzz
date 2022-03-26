96513c8d40e5848009fdc281eacd7c148d08dea9
Dimitar Uzunov <D.S.Uzunov-1@student.tudelft.nl>
2022-03-26 13:17:49 +0100

    Added settings screen and a dark image

--- client/src/main/java/client/scenes/SettingsCtrl.java
+++ client/src/main/java/client/scenes/SettingsCtrl.java
@@ -4,8 +4,14 @@ import client.MyFXML;
 import client.utils.SceneController;
 import com.google.inject.Inject;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Background;
+
+import java.util.EventListener;
 
 public class SettingsCtrl extends SceneController {
+    @FXML
+    Button colourModeButton;
 
     /**
      * Basic constructor.
@@ -28,4 +34,28 @@ public class SettingsCtrl extends SceneController {
     private void adminAction() {
         myFxml.showScene(AdminCtrl.class);
     }
+    /**
+     * Function called by Back to Mani menu button when clicked. Changes scene to SplashScreen scene.
+     */
+    @FXML
+    private void SplashAction() {
+        myFxml.showScene(SplashCtrl.class);
+    }
+
+    /**
+     * Function called by admin button when clicked. Changes scene to AdminPage scene.
+     */
+    @FXML
+    private void ChangeColourModeAction() {
+        if(colourModeButton.getText().equals("Dark Mode")){
+            colourModeButton.setText("Light Mode");
+        }else{
+            colourModeButton.setText("Dark Mode");
+        }
+
+
+
+
+    }
+
 }
