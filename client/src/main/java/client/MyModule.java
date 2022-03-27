8f21bcf9bccd412eef3b1cb515b741dd679ae19f
Alexandru Cristian Cazacu <a.c.cazacu@student.tudelft.nl>
2022-03-27 06:53:03 +0200

    Hotfix

--- client/src/main/java/client/MyModule.java
+++ client/src/main/java/client/MyModule.java
@@ -26,6 +26,9 @@ public class MyModule implements Module {
         binder.bind(SplashCtrl.class).in(Scopes.SINGLETON);
         binder.bind(WaitingRoomCtrl.class).in(Scopes.SINGLETON);
         binder.bind(QuestionTypeAComponentCtrl.class).in(Scopes.SINGLETON);
+        binder.bind(QuestionTypeBComponentCtrl.class).in(Scopes.SINGLETON);
+        binder.bind(QuestionTypeCComponentCtrl.class).in(Scopes.SINGLETON);
+        binder.bind(QuestionTypeDComponentCtrl.class).in(Scopes.SINGLETON);
         // other
 
     }
