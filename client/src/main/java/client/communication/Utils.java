6a32c9100cfd2f87924ade0ff2b50878d4277871
Dimitar Uzunov <D.S.Uzunov-1@student.tudelft.nl>
2022-03-31 19:53:32 +0200

    QuickSave to be able to test without laying a game
    The graph works but I swear to got I can not sort it

--- client/src/main/java/client/communication/Utils.java
+++ client/src/main/java/client/communication/Utils.java
@@ -5,6 +5,9 @@ import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
 import commons.User;
 import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.core.GenericType;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import org.glassfish.jersey.client.ClientConfig;
@@ -18,18 +21,33 @@ public class Utils {
      * @return Optional list of Users
      */
     public static Optional<List<User>> getAllUsers(String gameCode) {
-        try {
-            if (gameCode == null) {
-                throw new IllegalArgumentException("'gameCode' may not be null!");
-            }
-            return Optional.ofNullable(ClientBuilder.newClient(new ClientConfig())
-                    .target(serverAddress).path("/api/game/getUsers/" + gameCode)
-                    .request(APPLICATION_JSON)
-                    .get(new GenericType<List<User>>() { // do not remove the explicit type!
-                    }));
-        } catch (Exception e) {
-            e.printStackTrace();
-            return Optional.empty();
-        }
+
+        var dodi = new User("Dodi");
+         dodi.addScore(30);
+        var geca = new User("Geca1");
+        geca.addScore(20);
+        var geca1 = new User("Geca13");
+        geca1.addScore(40);
+        var geca3 = new User("Geca2");
+        geca3.addScore(50);
+        var geca2 = new User("Geca4");
+        geca2.addScore(40);
+
+
+        return Optional.of(Arrays.asList(dodi,geca,geca1,geca2,geca3));
+
+//        try {
+//            if (gameCode == null) {
+//                throw new IllegalArgumentException("'gameCode' may not be null!");
+//            }
+//            return Optional.ofNullable(ClientBuilder.newClient(new ClientConfig())
+//                    .target(serverAddress).path("/api/game/getUsers/" + gameCode)
+//                    .request(APPLICATION_JSON)
+//                    .get(new GenericType<List<User>>() { // do not remove the explicit type!
+//                    }));
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return Optional.empty();
+//        }
     }
 }
--- client/src/main/java/client/scenes/EndLeaderboardCtrl.java
+++ client/src/main/java/client/scenes/EndLeaderboardCtrl.java
@@ -1,4 +1,91 @@
 package client.scenes;
 
-public class EndLeaderboardCtrl {
+import static client.scenes.MainCtrl.currentGameID;
+import static client.scenes.MainCtrl.scheduler;
+
+import client.MyFXML;
+import client.communication.Utils;
+import client.communication.WaitingRoomCommunication;
+import client.utils.SceneController;
+import client.utils.UserAlert;
+import com.google.inject.Inject;
+import commons.User;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+
+/**
+ * Displays the final screen at the end of the game
+ */
+public class EndLeaderboardCtrl extends SceneController {
+
+
+
+    @FXML
+    private BarChart chart;
+    @FXML
+    private CategoryAxis usernames;
+    @FXML
+    private NumberAxis points;
+
+
+
+
+    /**
+     * Constructor used by INJECTOR.
+     * @param myFxml handled by INJECTOR
+     */
+    @Inject
+    protected EndLeaderboardCtrl(MyFXML myFxml) {
+        super(myFxml);
+
+    }
+
+    @Override
+    public void show() {
+
+         //get player list from server
+        scheduler.execute(() -> {
+            Optional<List<User>> l = Utils.getAllUsers(currentGameID);
+
+
+            if (l.isPresent()) {
+
+
+                var series = new XYChart.Series<String, Integer>();
+                series.setName("Points");
+                l.get().forEach(user -> {
+                    var data = new XYChart.Data<String,Integer>(user.getUsername(), user.getScore());
+                    series.getData().add(data);
+                });
+                //series.getData().stream().sorted((a,b)->a.getYValue().compareTo(b.getYValue())).collect(Collectors.toList());
+                System.out.println(series);
+
+                chart.getData().add(series);
+
+
+            }
+        });
+        present();
+
+
+    }
+
+
+
+
+    @FXML
+    private void onBackButton() {
+        myFxml.showScene(SplashCtrl.class);
+    }
 }
+
