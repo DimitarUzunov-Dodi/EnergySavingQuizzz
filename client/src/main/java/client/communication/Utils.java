3c30ae89e9f906795bedc16293233e55986664ab
Dimitar Uzunov <D.S.Uzunov-1@student.tudelft.nl>
2022-04-03 22:45:46 +0200

    Fix IDK

--- client/src/main/java/client/communication/Utils.java
+++ client/src/main/java/client/communication/Utils.java
@@ -5,9 +5,6 @@ import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
 import commons.User;
 import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.core.GenericType;
-
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import org.glassfish.jersey.client.ClientConfig;
@@ -22,32 +19,19 @@ public class Utils {
      */
     public static Optional<List<User>> getAllUsers(String gameCode) {
 
-        var dodi = new User("Dodi");
-         dodi.addScore(30);
-        var geca = new User("Geca");
-        geca.addScore(20);
-        var geca1 = new User("Geca1");
-        geca1.addScore(70);
-        var geca3 = new User("Geca3");
-        geca3.addScore(50);
-        var geca2 = new User("Geca2");
-        geca2.addScore(40);
 
-
-        return Optional.of(Arrays.asList(dodi,geca,geca1,geca2,geca3));
-
-//        try {
-//            if (gameCode == null) {
-//                throw new IllegalArgumentException("'gameCode' may not be null!");
-//            }
-//            return Optional.ofNullable(ClientBuilder.newClient(new ClientConfig())
-//                    .target(serverAddress).path("/api/game/getUsers/" + gameCode)
-//                    .request(APPLICATION_JSON)
-//                    .get(new GenericType<List<User>>() { // do not remove the explicit type!
-//                    }));
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            return Optional.empty();
-//        }
+        try {
+            if (gameCode == null) {
+                throw new IllegalArgumentException("'gameCode' may not be null!");
+            }
+            return Optional.ofNullable(ClientBuilder.newClient(new ClientConfig())
+                    .target(serverAddress).path("/api/game/getUsers/" + gameCode)
+                    .request(APPLICATION_JSON)
+                    .get(new GenericType<List<User>>() { // do not remove the explicit type!
+                    }));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Optional.empty();
+        }
     }
 }
